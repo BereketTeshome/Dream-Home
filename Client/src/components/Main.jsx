3fea7b254be8d377bf638e40490e03c38890ea60
Bereket <bereketteshome685@gmail.com>
2023-09-22 02:53:36 -0700

    search bug fixed

--- Client/src/components/Main.jsx
+++ Client/src/components/Main.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Home from './Home'
-import Section from './Section'
-import Footer from './Footer'
-import Contact from './Contact'
+import {React, lazy, Suspense} from 'react'
+const Home = lazy(() => import('./Home'))
+const Section = lazy(() => import('./Section'))
+const Footer = lazy(() => import('./Footer'))
+const Contact = lazy(() => import('./Contact'))
+
 
 const Main = () => {
   return (
     <div>
-      <Home />
-      <Section />
-      <Contact />
-      <Footer />
+      <Suspense>
+        <Home />
+        <Section />
+        <Contact />
+        <Footer />
+      </Suspense>
     </div>
   )
 }
